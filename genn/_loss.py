fixed bugs with normalization and mini-batch

genn/_loss.py
@@ -115,13 +115,14 @@ def cost(W: List[np.ndarray], b: List[np.ndarray], a: List[str],
         db = d(cost)/db = derivative of cost w.r.t. neural net biases
 
     """
+
     # Predict
     Y_pred, caches = L_model_forward(X, W, b, a)
     J_pred, J_caches = L_grads_forward(X, W, b, a)
 
     # Cost function
     c = 0
-    c = c + squared_loss(Y_pred, Y)
+    c = c + squared_loss(Y, Y_pred)
     c = c + regularization(W, X.shape[1], lambd)
     if J is not None:
         c = c + gradient_enhancement(J_pred, J, gamma)

genn/_model.py
@@ -28,7 +28,7 @@ class GENN:
                  solver: str = 'adam',
                  alpha: float = 0.0001,
                  gamma: int = 1,
-                 batch_size: int or str = 'auto',
+                 batch_size: int = None,
                  learning_rate: str = "constant",
                  learning_rate_init: float = 0.001,
                  num_epochs: int = 1,
@@ -269,11 +269,13 @@ class GENN:
             if X.shape[0] != Y.shape[0]:
                 raise ValueError(msg)
 
+        # Transpose
         X = X.T
         Y = Y.T
         if J is not None:
             J = J.T
 
+        # Normalize
         self._mu_x = np.array([[0]])
         self._sigma_x = np.array([[1]])
         self._mu_y = np.array([[0]])
@@ -354,8 +356,6 @@ class GENN:
             J = np.zeros((n_y, n_x, m))
 
         batch_size = self.batch_size
-        if self.batch_size == 'auto':
-            batch_size = min(200, m)
 
         for e in range(self.num_epochs):
             batches = mini_batches(X, batch_size, seed=self.random_state,
@@ -507,8 +507,8 @@ class GENN:
         Y_norm = L_model_forward(X_norm, self._W, self._b, self._a,
                                  store_cache=False)
 
-        Y = (self._sigma_y + EPS) * Y_norm + self._mu_y
-        return Y.T
+        Y = (self._sigma_y + EPS) * Y_norm.T + self._mu_y
+        return Y
 
     def gradient(self, X: np.ndarray):
         """
@@ -544,7 +544,10 @@ class GENN:
         J_norm = L_grads_forward(X_norm, self._W, self._b, self._a,
                                  store_cache=False)
 
-        J = J_norm * self._sigma_y / self._sigma_x
+        J = np.zeros(J_norm.shape)
+        for i in range(0, self._n_y):
+            for j in range(0, self._n_x):
+                J[i, j] = J_norm[i, j] * self._sigma_y[i] / self._sigma_x[j]
 
         return J.T
 