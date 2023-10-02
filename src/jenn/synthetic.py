868f24c74f73fdf1ad0cfb7d4887c259e5a2d4c0
Steven Berguin <shb@Stevens-MacBook-Air.local>
2023-10-02 10:29:31 -0400

    saving progress

--- src/jenn/synthetic.py
+++ src/jenn/synthetic.py
@@ -1,14 +1,25 @@
-"""Generate synthetic data from cannonical test function."""
+"""Canonical test function representative of topology
+encountered for surrogate-based optimization."""
+
+import abc
 import numpy as np
 
 from typing import Tuple
 
 
-class Sinusoid:
-    """y =x * np.sin(x)"""
+def _fullfact(n_x: int, m_levels: int):
+    """Return full factorial with sample values between 0 and 1."""
+    array = np.linspace(0, 1, m_levels)
+    arrays = [array] * n_x
+    meshes = [mesh.reshape((1, -1)) for mesh in np.meshgrid(*arrays)]
+    return np.concatenate(meshes)
 
-    @classmethod
-    def evaluate(cls, x: np.ndarray) -> np.ndarray:
+
+class TestFunction:
+    """Test function base class"""
+
+    @abc.abstractmethod
+    def evaluate(self, x: np.ndarray) -> np.ndarray:
         """Evaluate function
 
         Parameters
@@ -21,10 +32,10 @@ class Sinusoid:
         y: np.ndarray
             Output array of shape (n_y, m)
         """
-        return x * np.sin(x)
+        raise NotImplementedError
 
-    @classmethod
-    def first_derivative(cls, x: np.ndarray) -> np.ndarray:
+    @abc.abstractmethod
+    def first_derivative(self, x: np.ndarray) -> np.ndarray:
         """Evaluate partial derivative
 
         Parameters
@@ -37,15 +48,204 @@ class Sinusoid:
         dydx: np.ndarray
             Output array of shape (n_y, n_x, m)
         """
-        return np.sin(x) + x * np.cos(x)
+        raise NotImplementedError
 
     @classmethod
     def sample(
-            cls, m: int, lb: float = -np.pi, ub: float = np.pi,
+            cls,
+            m_lhs: int,
+            m_levels: int,
+            lb: np.ndarray or float,
+            ub: np.ndarray or float,
     ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
-        """Generate synthetic data"""
-        n_x = n_y = 1
-        x = np.linspace(lb, ub, m).reshape((n_x, m))
-        y = cls.evaluate(x).reshape((n_y, m))
-        dydx = cls.first_derivative(x).reshape((n_y, n_x, m))
+        """Generate synthetic data by sampling test function."""
+        lb = np.array([lb]).reshape((-1, 1))  # make sure it's an numpy array
+        ub = np.array([ub]).reshape((-1, 1))  # make sure it's an numpy array
+        n_x = lb.size
+        lh = np.random.rand(n_x, m_lhs)
+        ff = _fullfact(n_x, m_levels)
+        doe = np.concatenate([lh, ff], axis=1)
+        m = doe.shape[1]
+        x = lb + (ub - lb) * doe
+        y = cls.evaluate(x).reshape((-1, m))
+        dydx = cls.first_derivative(x).reshape((-1, n_x, m))
         return x, y, dydx
+
+
+class Linear(TestFunction):
+    """Linear test function:
+                         n
+    f(x1, ..., xn) = b + ∑ a[i] * x[i]
+                        i=0
+    """
+
+    @classmethod
+    def evaluate(
+            cls, x: np.ndarray,
+            a: float or np.ndarray = 1.0,
+            b: float = 0.0,
+    ) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        y = np.zeros((n_y, m))
+        y[:] = a * np.sum(x, axis=0) + b
+        return y
+
+    @classmethod
+    def first_derivative(
+            cls, x: np.ndarray,
+            a: float or np.ndarray = 1.0,
+            b: float = 0.0,
+    ) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        dydx = np.zeros((n_y, n_x, m))
+        a = np.array([a] * n_x) if isinstance(a, float) else a
+        for i in range(n_x):
+            dydx[0, i, :] = a[i]
+        return dydx
+
+    @classmethod
+    def sample(
+            cls,
+            m_lhs: int = 100,
+            m_levels: int = 0,
+            lb: np.ndarray or float = -1.,
+            ub: np.ndarray or float = 1.0,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        return super().sample(m_lhs, m_levels, lb, ub)
+
+
+class Parabola(TestFunction):
+    """Parabola test function:
+
+                           n
+    f(x1, ..., xn) = 1/n * ∑ (x[i] - x0[i]) ** 2
+                          i=0
+
+    """
+
+    @classmethod
+    def evaluate(cls, x: np.ndarray, x0: np.ndarray = 0.0) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        y = np.zeros((n_y, m))
+        y[:] = 1 / n_x * np.sum((x - x0) ** 2, axis=0)
+        return y
+
+    @classmethod
+    def first_derivative(
+            cls, x: np.ndarray, x0: np.ndarray = 0.0) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        dydx = np.zeros((n_y, n_x, m))
+        dydx[0, :, :] = 2 / n_x * (x - x0)
+        return dydx
+
+    @classmethod
+    def sample(
+            cls,
+            m_lhs: int = 100,
+            m_levels: int = 0,
+            lb: np.ndarray or float = -1.,
+            ub: np.ndarray or float = 1.0,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        return super().sample(m_lhs, m_levels, lb, ub)
+
+
+class Sinusoid(TestFunction):
+    """Sinusoidal test function: y =x * np.sin(x)"""
+
+    @classmethod
+    def evaluate(cls, x: np.ndarray) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        y = np.zeros((n_y, m))
+        y[:] = x * np.sin(x)
+        return y
+
+    @classmethod
+    def first_derivative(cls, x: np.ndarray) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        dydx = np.zeros((n_y, n_x, m))
+        dydx[0, 0, :] = np.sin(x) + x * np.cos(x)
+        return dydx
+
+    @classmethod
+    def sample(
+            cls,
+            m_lhs: int = 100,
+            m_levels: int = 0,
+            lb: np.ndarray or float = -np.pi,
+            ub: np.ndarray or float = np.pi,
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        return super().sample(m_lhs, m_levels, lb, ub)
+
+
+class Rastrigin(TestFunction):
+    """Rastrigin (egg crate) test function:
+                     n
+    f(x1, ..., xn) = ∑ (x[i] ** 2 − 10 cos(2πxi))
+                    i=0
+    """
+
+    @classmethod
+    def evaluate(cls, x: np.ndarray) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        y = np.zeros((n_y, m)) + 10 * n_x
+        for i in range(n_x):
+            y += np.power(x[i], 2) - 10 * np.cos(2 * np.pi * x[i])
+        return y
+
+    @classmethod
+    def first_derivative(cls, x: np.ndarray) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        dydx = np.zeros((n_y, n_x, m))
+        for i in range(n_x):
+            dydx[0, i, :] = 2 * x[i] + 20 * np.pi * np.sin(2 * np.pi * x[i])
+        return dydx
+
+    @classmethod
+    def sample(
+            cls,
+            m_lhs: int = 100,
+            m_levels: int = 0,
+            lb: np.ndarray or float = -1. * np.ones(2,),
+            ub: np.ndarray or float = 1.5 * np.ones(2,),
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        return super().sample(m_lhs, m_levels, lb, ub)
+
+
+class Rosenbrock(TestFunction):
+    """Banana Rosenbrock test function:
+    y = (1 - x1) ** 2 + 100 * (x2 - x1 ** 2) ** 2"""
+
+    @classmethod
+    def evaluate(cls, x: np.ndarray) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        y = np.zeros((n_y, m))
+        y[:] = (1 - x[0]) ** 2 + 100 * (x[1] - x[0] ** 2) ** 2
+        return y
+
+    @classmethod
+    def first_derivative(cls, x: np.ndarray) -> np.ndarray:
+        n_y = 1
+        n_x, m = x.shape
+        dydx = np.zeros((n_y, n_x, m))
+        dydx[0, 0, :] = -2 * (1 - x[0]) - 400 * x[0] * (x[1] - x[0] ** 2)
+        dydx[0, 1, :] = 200 * (x[1] - x[0] ** 2)
+        return dydx
+
+    @classmethod
+    def sample(
+            cls,
+            m_lhs: int = 100,
+            m_levels: int = 0,
+            lb: np.ndarray or float = -2 * np.ones(2,),
+            ub: np.ndarray or float = 2. * np.ones(2,),
+    ) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        return super().sample(m_lhs, m_levels, lb, ub)
